3521ae896e6912c4d3b9ea1be2bf0abb69a9cf37
Zach Howarth <zachhowarth@gmail.com>
2019-07-12 11:12:33 -0500

    Commit 2

--- emojinal/ViewController.swift
+++ emojinal/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var broccoli: UIButton!
+    
+    @IBOutlet weak var donut: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func showMessage(sender: UIButton) {
+        
+        let alertController = UIAlertController(title: "Broccoli", message: "Good Choice. You will live long.", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "Um thx?", style: UIAlertAction.Style.default, handler: nil))
+        self.present(alertController, animated: true, completion: nil)
+        
+        
+    }
 
 }
 
