978b1cbb7b3a3665a815bd4f205a9f334da165f7
Zach Howarth <zachhowarth@gmail.com>
2019-07-12 13:12:44 -0500

    Done Commit

--- emojinal/ViewController.swift
+++ emojinal/ViewController.swift
@@ -10,9 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var broccoli: UIButton!
+    let emojis = ["🍩" : "donut", "🥦" : "broccoli"]
     
-    @IBOutlet weak var donut: UIButton!
+    let customMessages = ["donut": ["Donut... very unhealthy you are." , "DONUT YUMMMMM YUM YUM!", "*Beware... in the next 24 hours, you may have a donut thrown at you.*", "You should eat some broccoli some time."],
+        "broccoli": ["Broccoli. Good choice, you will live long.", "Broccoli... are you sure you chose correctly? JK broccoli is always the right choice.", "You deserve a donut.", "Yas broccoli!"]]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,16 +22,12 @@ class ViewController: UIViewController {
     
     @IBAction func showMessage(sender: UIButton) {
         let selectedEmotion = sender.titleLabel?.text
-        let emojis = ["🍩" : "donut", "🥦" : "broccoli"]
+  
         
-        "donut"; ["Donut... very unhealthy you are." , "DONUT YUMMMMM YUM YUM!", "*Beware... in the next 24 hours, you may have a donut thrown at you.*", "You should eat some broccoli some time."]
+        let emojiMessage = customMessages[emojis[selectedEmotion!]!]?[Int.random(in: 0...3)]
         
-        "broccoli"; ["Broccoli. Good choice, you will live long.", "Broccoli... are you sure you chose correctly? JK broccoli is always the right choice.", "You deserve a donut."]
-        
-        let emojiMessage = emojis[selectedEmotion!]
-
-        let alertController = UIAlertController(title: "Food Choice", message: emojis[selectedEmotion!], preferredStyle: UIAlertController.Style.alert)
-        alertController.addAction(UIAlertAction(title: "Sure...", style: UIAlertAction.Style.default, handler: nil))
+        let alertController = UIAlertController(title: "Food Choice", message: emojiMessage, preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "Sure", style: UIAlertAction.Style.default, handler: nil))
         self.present(alertController, animated: true, completion: nil)
         
         
